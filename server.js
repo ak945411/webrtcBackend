3c8d41b4c689fbd72bf49abc92ec6dda1cb39940
ANSH KAUSHIK <anshkaushik9454@gmail.com>
2024-12-10 23:48:21 +0530

    cors fixes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cors({ origin: ["http://localhost:5173", "http://192.168.0.105:5173", "http:sangya.web.app"] })
+  cors({ origin: ["http://localhost:5173", "http://192.168.0.105:5173", "https:sangya.web.app"] })
 );
 
 app.get("/stream", (req, res) => {
